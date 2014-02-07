4005f2823508db2c4d8850fb885c869ef36c7084
Simon Jagoe <simon@simonjagoe.com>
2014-02-07 22:26:36 +0000

    Fix coverage

--- haas/__init__.py
+++ haas/__init__.py
@@ -1,5 +1,5 @@
 # Copyright 2013-2014 Simon Jagoe
 try:
     from haas.version import version as __version__
-except ImportError:
+except ImportError:  # pragma: no cover
     __version__ = 'notset'
