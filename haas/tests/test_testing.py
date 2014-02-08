db083ac4afd9c3967a1e59240ceb1273772a299d
Simon Jagoe <simon@simonjagoe.com>
2014-02-08 11:51:28 +0000

    Fixed expected_failure context manager tests

--- haas/tests/test_testing.py
+++ haas/tests/test_testing.py
@@ -1,20 +1,37 @@
 # Copyright 2013-2014 Simon Jagoe
-from ..testing import unittest, expected_failure
+from ..testing import (
+    _ExpectedFailure,
+    _UnexpectedSuccess,
+    expected_failure,
+    unittest,
+)
 
 
 class TestExpectedFailureContextManager(unittest.TestCase):
 
-    def test_decorator_exception(self):
-        with expected_failure():
-            raise Exception()
+    def test_context_exception(self):
+        try:
+            with expected_failure():
+                raise Exception()
+        except _ExpectedFailure:
+            return
+        self.fail('Did not receive expected failure')
 
-    def test_decorator_failure(self):
-        with expected_failure():
-            self.fail()
+    def test_context_failure(self):
+        try:
+            with expected_failure():
+                self.fail()
+        except _ExpectedFailure:
+            return
+        self.fail('Did not receive expected failure')
 
     def test_decorator_unexpected_success(self):
-        with expected_failure():
-            pass
+        try:
+            with expected_failure():
+                pass
+        except _UnexpectedSuccess:
+            return
+        self.fail('Did not receive unexpected success')
 
     def test_decorator_success(self):
         with expected_failure(False):
