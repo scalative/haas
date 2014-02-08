2b69aff01b80b8b98f45cba54f23aa80e370f63f
Simon Jagoe <simon@simonjagoe.com>
2014-02-08 11:16:07 +0000

    Add expected failure contextmanager

--- haas/testing.py
+++ haas/testing.py
@@ -1,8 +1,20 @@
 # Copyright 2013-2014 Simon Jagoe
+from contextlib import contextmanager
 import sys
 
 
 if sys.version_info[:2] == (2, 6):  # pragma: no cover
     import unittest2 as unittest
+    from unittest2.case import _ExpectedFailure, _UnexpectedSuccess
 else:
     import unittest
+    from unittest.case import _ExpectedFailure, _UnexpectedSuccess
+
+
+@contextmanager
+def expected_failure():
+    try:
+        yield
+    except Exception:
+        raise _ExpectedFailure(sys.exc_info())
+    raise _UnexpectedSuccess()
---
+++ haas/tests/test_testing.py
@@ -0,0 +1,17 @@
+# Copyright 2013-2014 Simon Jagoe
+from ..testing import unittest, expected_failure
+
+
+class TestExpectedFailureContextManager(unittest.TestCase):
+
+    def test_decorator_exception(self):
+        with expected_failure():
+            raise Exception()
+
+    def test_decorator_failure(self):
+        with expected_failure():
+            self.fail()
+
+    def test_decorator_unexpected_success(self):
+        with expected_failure():
+            pass
