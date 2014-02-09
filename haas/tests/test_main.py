55484554eca2ad6340f840cdbf2245402143b4bf
Simon Jagoe <simon@simonjagoe.com>
2014-02-09 14:07:38 +0000

    Add options to be compatible with unittest

--- haas/tests/test_main.py
+++ haas/tests/test_main.py
@@ -16,18 +16,78 @@ from ..testing import unittest
 
 class TestMain(unittest.TestCase):
 
-    @patch('haas.testing.unittest.TextTestRunner')
-    def test_main_runs_tests(self, runner_class):
+    def _run_with_arguments(self, runner_class, *args):
         runner = Mock()
         runner_class.return_value = runner
 
         result = Mock()
         result.wasSuccessful = Mock()
-        run = Mock(return_value=result)
-        runner.run = run
+        run_method = Mock(return_value=result)
+        runner.run = run_method
 
-        main(['argv0', 'haas'])
-        runner_class.assert_called_once_with()
+        main(['argv0'] + list(args) + ['haas'])
+        return run_method, result
+
+    @patch('haas.testing.unittest.TextTestRunner')
+    def test_main_default_arguments(self, runner_class):
+        run, result = self._run_with_arguments(runner_class)
+        runner_class.assert_called_once_with(
+            verbosity=1, failfast=False, buffer=False)
+        suite = Discoverer(Loader()).discover('haas')
+        run.assert_called_once_with(suite)
+
+        result.wasSuccessful.assert_called_once_with()
+
+    @patch('haas.testing.unittest.TextTestRunner')
+    def test_main_quiet(self, runner_class):
+        run, result = self._run_with_arguments(runner_class, '-q')
+
+        runner_class.assert_called_once_with(
+            verbosity=0, failfast=False, buffer=False)
+        suite = Discoverer(Loader()).discover('haas')
+        run.assert_called_once_with(suite)
+
+        result.wasSuccessful.assert_called_once_with()
+
+    @patch('haas.testing.unittest.TextTestRunner')
+    def test_main_quiet_and_verbose(self, runner_class):
+        run, result = self._run_with_arguments(runner_class, '-q', '-v')
+
+        runner_class.assert_called_once_with(
+            verbosity=1, failfast=False, buffer=False)
+        suite = Discoverer(Loader()).discover('haas')
+        run.assert_called_once_with(suite)
+
+        result.wasSuccessful.assert_called_once_with()
+
+    @patch('haas.testing.unittest.TextTestRunner')
+    def test_main_verbose(self, runner_class):
+        run, result = self._run_with_arguments(runner_class, '-v')
+
+        runner_class.assert_called_once_with(
+            verbosity=2, failfast=False, buffer=False)
+        suite = Discoverer(Loader()).discover('haas')
+        run.assert_called_once_with(suite)
+
+        result.wasSuccessful.assert_called_once_with()
+
+    @patch('haas.testing.unittest.TextTestRunner')
+    def test_main_failfast(self, runner_class):
+        run, result = self._run_with_arguments(runner_class, '-f')
+
+        runner_class.assert_called_once_with(
+            verbosity=1, failfast=True, buffer=False)
+        suite = Discoverer(Loader()).discover('haas')
+        run.assert_called_once_with(suite)
+
+        result.wasSuccessful.assert_called_once_with()
+
+    @patch('haas.testing.unittest.TextTestRunner')
+    def test_main_buffer(self, runner_class):
+        run, result = self._run_with_arguments(runner_class, '-b')
+
+        runner_class.assert_called_once_with(
+            verbosity=1, failfast=False, buffer=True)
         suite = Discoverer(Loader()).discover('haas')
         run.assert_called_once_with(suite)
 
