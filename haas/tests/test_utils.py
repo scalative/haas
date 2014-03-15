bea27dc5b75fcd4c3c62348d688e184efa4c5a4f
Simon Jagoe <simon@simonjagoe.com>
2014-03-15 22:17:20 +0200

    Fixed import order

--- haas/tests/test_utils.py
+++ haas/tests/test_utils.py
@@ -4,14 +4,12 @@
 #
 # This software may be modified and distributed under the terms
 # of the 3-clause BSD license.  See the LICENSE.txt file for details.
-from __future__ import unicode_literals
-
-import haas
+from __future__ import absolute_import, unicode_literals
 
 from mock import patch
 
-from haas.testing import unittest
-
+import haas
+from ..testing import unittest
 from ..utils import configure_logging
 
 
