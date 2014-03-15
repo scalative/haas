d3e60ee698e61d25644b1c0052336318a8cb41f1
Simon Jagoe <simon@simonjagoe.com>
2014-03-15 23:00:09 +0200

    Add from __future__ import absolute_import

--- haas/tests/__init__.py
+++ haas/tests/__init__.py
@@ -4,3 +4,4 @@
 #
 # This software may be modified and distributed under the terms
 # of the 3-clause BSD license.  See the LICENSE.txt file for details.
+from __future__ import absolute_import, unicode_literals
