38556305bd2498bb1ffdd28df8d59ca6886f5897
Simon Jagoe <simon@simonjagoe.com>
2014-08-31 12:37:50 +0100

    Improve haas setup.py

--- setup.py
+++ setup.py
@@ -6,69 +6,123 @@
 # of the 3-clause BSD license.  See the LICENSE.txt file for details.
 import os
 import sys
+import subprocess
 
 from setuptools import setup
 
 
-VERSION = '0.5.0.dev'
+MAJOR = 0
+MINOR = 5
+MICRO = 0
+
+IS_RELEASED = False
+
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
-def write_version_py(filename=None):
-    if filename is None:
-        filename = os.path.join(os.path.dirname(__file__),
-                                'haas', 'version.py')
-    ver = """\
-version = {version!r}
-"""
-    fh = open(filename, 'w')
+# Return the git revision as a string
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, env=env,
+        ).communicate()[0]
+        return out
+
     try:
-        fh.write(ver.format(version=VERSION))
-    finally:
-        fh.close()
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        git_revision = out.strip().decode('ascii')
+    except OSError:
+        git_revision = "Unknown"
+
+    return git_revision
 
 
-write_version_py()
+def write_version_py(filename='haas/_version.py'):
+    template = """\
+# THIS FILE IS GENERATED FROM SETUP.PY
+version = '{version}'
+full_version = '{full_version}'
+git_revision = '{git_revision}'
+is_released = {is_released}
+
+if not is_released:
+    version = full_version
+"""
+    fullversion = VERSION
+    if os.path.exists('.git'):
+        git_rev = git_version()
+    elif os.path.exists('haas/_version.py'):
+        # must be a source distribution, use existing version file
+        try:
+            from haas._version import git_revision as git_rev
+        except ImportError:
+            raise ImportError("Unable to import git_revision. Try removing "
+                              "haas/_version.py and the build "
+                              "directory before building.")
+    else:
+        git_rev = "Unknown"
+
+    if not IS_RELEASED:
+        fullversion += '.dev1-' + git_rev[:7]
+
+    with open(filename, "wt") as fp:
+        fp.write(template.format(version=VERSION,
+                                 full_version=fullversion,
+                                 git_revision=git_rev,
+                                 is_released=IS_RELEASED))
 
 
-with open('README.rst') as fh:
-    long_description = fh.read()
+if __name__ == "__main__":
+    if sys.version_info < (2, 7):
+        setup_kwargs = {
+            'install_requires': ['unittest2', 'argparse'],
+        }
+    else:
+        setup_kwargs = {}
 
+    write_version_py()
+    from haas import __version__
 
-if sys.version_info < (2, 7):
-    setup_kwargs = {
-        'install_requires': ['unittest2', 'argparse'],
-    }
-else:
-    setup_kwargs = {}
+    with open('README.rst') as fh:
+        long_description = fh.read()
 
-
-setup(
-    name='haas',
-    version=VERSION,
-    url='https://github.com/sjagoe/haas',
-    author='Simon Jagoe',
-    author_email='simon@simonjagoe.com',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: MacOS',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: OS Independent',
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
-        'Programming Language :: Python',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Testing',
-    ],
-    description='Extensible Python Test Runner',
-    long_description=long_description,
-    license='BSD',
-    packages=['haas'],
-    entry_points={
-        'console_scripts': [
-            'haas=haas.main:main',
+    setup(
+        name='haas',
+        version=__version__,
+        url='https://github.com/sjagoe/haas',
+        author='Simon Jagoe',
+        author_email='simon@simonjagoe.com',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: MacOS',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: OS Independent',
+            'Operating System :: POSIX',
+            'Operating System :: Unix',
+            'Programming Language :: Python',
+            'Topic :: Software Development',
+            'Topic :: Software Development :: Testing',
         ],
-    },
-    **setup_kwargs
-)
+        description='Extensible Python Test Runner',
+        long_description=long_description,
+        license='BSD',
+        packages=['haas'],
+        entry_points={
+            'console_scripts': [
+                'haas=haas.main:main',
+            ],
+        },
+        **setup_kwargs
+    )
