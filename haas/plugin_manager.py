cf7c906f5d146b6d5b8909f7859cc7841a908781
Simon Jagoe <simon@simonjagoe.com>
2014-10-28 16:27:27 +0000

    Add infrastructure for "driver" style plugins (of which only one instance is allowed)

--- haas/plugin_manager.py
+++ haas/plugin_manager.py
@@ -25,6 +25,8 @@ class PluginManager(object):
             invoke_on_load=True,
         )
 
+        self.driver_managers = OrderedDict()
+
     @classmethod
     def testing_plugin_manager(cls, hook_managers):
         plugin_manager = cls.__new__(cls)
@@ -42,11 +44,25 @@ class PluginManager(object):
     def _configure_hook_extension(self, extension, args):
         extension.obj.configure(args)
 
+    def _namespace_to_option(self, namespace):
+        parts = namespace.split('.')
+        option = '--{}'.format('-'.join(parts))
+        dest = '_'.join(parts)
+        return option, dest
+
     def add_plugin_arguments(self, parser):
         for manager in self.hook_managers.values():
             if len(list(manager)) == 0:
                 continue
             manager.map(self._add_hook_extension_arguments, parser)
+        for namespace, manager in self.driver_managers.items():
+            choices = list(sorted(manager.names()))
+            if len(choices) == 0:
+                continue
+            option, dest = self._namespace_to_option(namespace)
+            parser.add_argument(
+                option, help=self._help[namespace], dest=dest,
+                choices=choices, default='default')
 
     def configure_plugins(self, args):
         for manager in self.hook_managers.values():
@@ -60,3 +76,11 @@ class PluginManager(object):
             return []
         return [plugin for plugin in manager.map(self._filter_enabled_plugins)
                 if plugin is not None]
+
+    def get_driver(self, namespace, args):
+        option, dest = self._namespace_to_option(namespace)
+        driver_name = getattr(args, dest, 'default')
+        driver_manager = DriverManager(
+            namespace, driver_name,
+        )
+        return driver_manager.driver
