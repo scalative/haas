13b928cc5cce9fce040f76ddf11b604bd5b20292
Simon Jagoe <simon@simonjagoe.com>
2014-11-27 20:32:39 +0000

    Build one wheel for py27, py32 and py33 as the dependencies are the same for all of them.

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 python setup.py sdist
 python setup.py bdist_wheel --python-tag py26
-python setup.py bdist_wheel --python-tag py27
-python setup.py bdist_wheel --python-tag py32
-python setup.py bdist_wheel --python-tag py33
+python setup.py bdist_wheel --python-tag py27.py32.py33
 python setup.py bdist_wheel --python-tag py34
