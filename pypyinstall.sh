8d126ce0e859688c5f06b1401b9c38d8aec54570
Simon Jagoe <simon@simonjagoe.com>
2016-04-09 12:32:28 +0300

    simplify

--- pypyinstall.sh
+++ pypyinstall.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-if python --version 2>&1 | grep PyPy >/dev/null; then
-    git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-    PYENV_ROOT="$HOME/.pyenv"
-    PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-    pyenv install "pypy-${1}"
-    pyenv global "pypy-${1}"
-    pypy --version
-fi
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+PYENV_ROOT="$HOME/.pyenv"
+PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+pyenv install "pypy-${1}"
+pyenv global "pypy-${1}"
+pypy --version
