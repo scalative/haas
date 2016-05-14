a00d775aadacad9da5e47aa54d79880d49029fe0
Ioannis Tziakos <ioannist@enthought.com>
2016-05-14 03:25:38 +0100

    fix issue #143

--- haas/loader.py
+++ haas/loader.py
@@ -6,7 +6,7 @@
 # of the 3-clause BSD license.  See the LICENSE.txt file for details.
 from __future__ import absolute_import, unicode_literals
 
-from .testing import unittest
+import unittest
 from .suite import TestSuite
 
 
