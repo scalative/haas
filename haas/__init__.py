e305e660fede7b0d7dc7d9ebf422fbe992023ac3
Simon Jagoe <simon@simonjagoe.com>
2023-12-03 22:02:59 +0200

    Add pyproject.toml

--- haas/__init__.py
+++ haas/__init__.py
@@ -6,12 +6,12 @@
 # of the 3-clause BSD license.  See the LICENSE.txt file for details.
 from __future__ import absolute_import, unicode_literals
 
+import logging
+
 try:
     from haas._version import version as __version__
 except ImportError:  # pragma: no cover
     __version__ = 'notset'
-
-import logging
 
 
 class NullHandler(logging.Handler):  # pragma: no cover
